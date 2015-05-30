82fb6e2669c6ac714bd96c12c1662e66e6456186
Tobias Wright <tobias.wright@gmail.com>
2015-05-30 01:03:51 -0400

    added initial images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,35 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+
+var gallery = document.getElementById("gallery")
+
+var txt = "";
+var images = [
+	"Slides_004",
+	"Slides_032",
+	"Slides_066",
+	"Slides_067",
+	"Slides_070",
+	"Slides_073",
+	"Slides_074",
+	"Slides_086",
+	"Slides_098",
+	"Slides_102",
+	"Slides_106",
+	"Slides_111",
+	"Slides_113",
+	"Slides_119",
+	"Slides_120",
+	"Slides_127",
+	"Slides_129",
+	"Slides_130",
+	"Slides_131",
+	"Slides_133"
+];
+
+var imageTag = "<img class='galleryimg' src='images/{image}.jpg' />";
+
+images.map( function (image) {
+	txt += imageTag.replace('{image}', image);
+});
+
+gallery.innerHTML = txt
