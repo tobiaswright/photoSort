dd6ec9c9fc42e6e11fa931f9dee080eb8476008e
Tobias Wright <tobias.wright@gmail.com>
2015-05-31 16:57:35 -0400

    dynamic nav setup

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,28 @@
 /* jshint devel:true */
 var gallery = document.getElementById("gallery");
+var nav = document.getElementById("nav");
+var modalimage = document.getElementById("modalimage");
 var srcImages = ["Slides_004","Slides_032","Slides_066","Slides_067","Slides_070","Slides_073","Slides_074","Slides_086","Slides_098","Slides_102","Slides_106","Slides_111","Slides_113","Slides_119","Slides_120","Slides_127","Slides_129","Slides_130","Slides_131","Slides_133"];
 var li = "";
+var navli = ""
+var sets = [
+		{
+			setName: "All Images",
+		}
+	];
 var kids;
+
+sets.map( function( nav ) {
+	navli += "<li>"+nav.setName+"</li>"
+
+})
+
+nav.innerHTML = navli;
 
 //drops in default images
 srcImages.map( function (image) {
 	li += "<li>";
-	li += "<div data-toggle='modal' data-target='#myModal'	></div>";
+	li += "<div data-toggle='modal' data-target='#myModal'></div>";
 	li += '<img class="galleryimg" data-img="'+image+'" src="images/'+image+'.jpg" />';
 	li += '<span class="glyphicon glyphicon glyphicon-ok-circle" aria-hidden="true"></span>';
 	li += "</li>";
@@ -21,6 +36,9 @@ for (var i = 0;i<kids.length;i++) {
 	kids[i].addEventListener('click', function(e) {
 		var slide = e.target.nextSibling.getAttribute('data-img');
 
+		var slideCont = '<img src="images/'+slide+'.jpg" />'
+
+		modalimage.innerHTML = slideCont
 
 	});
 }
